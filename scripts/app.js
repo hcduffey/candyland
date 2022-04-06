b8df63a0692e3baca217ef474bd68eb651282933
hcduffey <cliff.duffey@pm.me>
2022-04-06 12:56:55 -0400

    create psuedocode for the JS file

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+// 1. Initialize the game and board
+// Create array of Square records that stores the state of each square on the gameboard
+// Set location of each piece to the start of the board.
+// Ensure no card is drawn
+// Set turn to Player 1 and register event handler for draw button
+
+// 2. Play a turn
+// User clicks the Draw button to invoke the event handler.
+// The handler will draw a card, and move to the players game piece to the corresponding square
+
+// 3. Check of Win
+// If the location of that players piece is at the end of the board state array, we have a winner and pop up a modal
+
+
+// 4. Switch turns
+// If there isn't a winner, switch to player two and listen for the Draw event for that player
+
+
